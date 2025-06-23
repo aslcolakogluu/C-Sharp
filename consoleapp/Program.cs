d2a33e472686a119a7d21ae133e5571eede4c978
Aslı Çolakoğlu <colakogluasli2@gmail.com>
2025-06-23 13:11:16 +0300

    Initial commit

---
+++ consoleapp/Program.cs
@@ -0,0 +1,29 @@
+﻿var ogrenci_ad = "Aslı";
+var ogrenci_soyad = "Çolakoğlu";
+var ogrenci_ad_soyad = ogrenci_ad + " " + ogrenci_soyad;
+var ogrenciNo = "210208026";
+var cinsiyet = "Kadın";
+var tc_kimlik = "4782827938";
+var dogum_yili = "2002";
+var adres = "Ankara";
+var yas = "23";
+
+Console.WriteLine(ogrenci_ad);
+Console.WriteLine(ogrenci_soyad);
+Console.WriteLine(ogrenci_ad_soyad);
+Console.WriteLine(ogrenciNo);
+Console.WriteLine(cinsiyet);
+Console.WriteLine(tc_kimlik);
+Console.WriteLine(dogum_yili);
+Console.WriteLine(adres);
+Console.WriteLine(yas);
+
+//*************************************************************//
+
+var urun1 = 50;
+var urun2 = 60.5;
+var urun3 = 356.45;
+
+var toplam = urun1 + urun2 + urun3;
+
+Console.WriteLine(toplam);
