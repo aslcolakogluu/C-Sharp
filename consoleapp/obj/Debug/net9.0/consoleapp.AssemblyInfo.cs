9785c9e9499d76d1e1dea2df5546896dcf0b66cf
Aslı Çolakoğlu <colakogluasli2@gmail.com>
2025-06-23 13:14:31 +0300

    Değişkenler

--- consoleapp/obj/Debug/net9.0/consoleapp.AssemblyInfo.cs
+++ consoleapp/obj/Debug/net9.0/consoleapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("consoleapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a33e472686a119a7d21ae133e5571eede4c978")]
 [assembly: System.Reflection.AssemblyProductAttribute("consoleapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("consoleapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
