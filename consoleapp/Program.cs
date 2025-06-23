631de0105e3c6af5eda4614fe7bc9e14c2b0e6ef
Aslı Çolakoğlu <colakogluasli2@gmail.com>
2025-06-23 13:39:13 +0300

    Veri Tipi Dönüşümü Düzenleme

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -30,12 +30,5 @@ Console.WriteLine(toplam);
 */
 //*************************************************************//
 
-Console.Write("1.Sayı: ");
-var sayi1 = Convert.ToInt32(Console.ReadLine()); //string to int
 
-Console.Write("2.Sayı: ");
-var sayi2 = Convert.ToInt32(Console.ReadLine()); //string to int
-
-var sum = sayi1 + sayi2;
-
-Console.WriteLine("Toplam: " + sum);
+ 
---
+++ consoleapp/veritipleri/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("1.Sayı: ");
+var sayi1 = Convert.ToInt32(Console.ReadLine()); //string to int
+
+Console.Write("2.Sayı: ");
+var sayi2 = Convert.ToInt32(Console.ReadLine()); //string to int
+
+var sum = sayi1 + sayi2;
+
+Console.WriteLine("Toplam: " + sum);
+//*************************************************************//
+
+int? maas = default;
+bool? isActive = null;
+
+Console.WriteLine(maas.HasValue);
+Console.WriteLine(maas.GetValueOrDefault());
+Console.WriteLine(isActive.GetValueOrDefault());
